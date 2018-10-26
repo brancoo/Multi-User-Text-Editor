820ce4ee2946f4d2ca2b5fd303c2ae48367c682a
Branco <jpeeeh@hotmail.com>
2018-10-26 16:51:20 +0100

    Update main.c

--- client/src/main.c
+++ client/src/main.c
@@ -119,7 +119,7 @@ int main(int argc, char **argv) {
     }
   }
 
-  if (user != NULL) {
+  if (strlen(user) == 0) {
       printf("Username: "); // senão existir então é pedido explicitamente
       scanf("%s", user);
   }
